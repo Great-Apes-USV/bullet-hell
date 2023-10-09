dcb2ebc6573ec244066985456e05f59466c56dd9
Chai Nunes <chaiyawahoo@gmail.com>
2023-10-09 10:44:08 -0700

    Initialize player movement

---
+++ scripts/player.gd
@@ -0,0 +1,16 @@
+extends CharacterBody2D
+
+@export var speed : float = 200
+
+var move_vector : Vector2 = Vector2.ZERO
+
+func _ready():
+	pass
+
+func _process(delta):
+	$Sprite2D.look_at(get_global_mouse_position())
+	move_vector = Input.get_vector("move-left", "move-right", "move-up", "move-down")
+
+func _physics_process(delta):
+	velocity = move_vector * speed
+	move_and_slide()
