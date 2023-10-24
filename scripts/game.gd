87ce4b056826ba3cd9860a745b73403a1326a80e
Chai Nunes <chaiyawahoo@gmail.com>
2023-10-24 12:06:58 -0700

    Tweaks, resizing fixed

--- scripts/game.gd
+++ scripts/game.gd
@@ -2,4 +2,12 @@ extends Node2D
 
 
 func _ready():
-	NavigationServer2D.map_set_cell_size($World.get_world_2d().navigation_map, 0.25)
+	get_viewport().size_changed.connect(_on_viewport_resize)
+
+
+func _on_viewport_resize():
+	var fake_size : Vector2i = get_viewport_transform().get_scale().x * $World.rect.size
+	var window_size : Vector2i = DisplayServer.window_get_size()
+	
+	position.x = ((window_size.x - fake_size.x) / 2) / get_viewport_transform().get_scale().x
+	position.y = ((window_size.y - fake_size.y) / 2) / get_viewport_transform().get_scale().y
