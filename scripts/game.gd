3214c0d62e8a76267e15b97f9be5766273ffccb5
Chai Nunes <chaiyawahoo@gmail.com>
2023-10-30 11:29:59 -0700

    Finish LDtk upload and resize

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,13 +1,34 @@
 extends Node2D
 
+@onready var viewport_scale : float = get_viewport_transform().get_scale().x # aspect ratio locked
+@onready var window_size : Vector2i = DisplayServer.window_get_size() / viewport_scale
+@onready var aspect_ratio : float = float(window_size.x) / window_size.y
+@onready var gap : Vector2i
+
 
 func _ready():
+	var diff : Vector2i = window_size - $World.rect.size
+	if diff.x > diff.y:
+		window_size *= float($World.rect.size.x) / window_size.x
+	else:
+		window_size *= float($World.rect.size.y) / window_size.y
+	gap = window_size - $World.rect.size
+	get_viewport().content_scale_size = window_size
+	position = (window_size - $World.rect.size) / 2
+	
 	get_viewport().size_changed.connect(_on_viewport_resize)
 
 
+func _process(_delta):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+
 func _on_viewport_resize():
-	var viewport_scale : float = get_viewport_transform().get_scale().x # aspect ratio locked
-	var window_size : Vector2i = DisplayServer.window_get_size() / viewport_scale
+	viewport_scale = get_viewport_transform().get_scale().x # aspect ratio locked
+	window_size = DisplayServer.window_get_size() / viewport_scale
 	
-	position.x = (window_size.x - $World.rect.size.x) / 2
-	position.y = (window_size.y - $World.rect.size.y) / 2
+	position = (window_size - $World.rect.size) / 2
