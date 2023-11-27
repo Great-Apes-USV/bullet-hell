b223e3331310c7b7f9431bceb232f7044870550d
Chai Nunes <chaiyawahoo@gmail.com>
2023-11-27 11:58:18 -0800

    Dungeon Worlds

---
+++ scripts/dungeon_world.gd
@@ -0,0 +1,20 @@
+extends LDTKWorld
+
+
+var layout : Array[Array] = [
+	[9, 15, 20],
+	[-1, 18, 8],
+	[-1, 11, 16],
+	[-1, 17, 19],
+	[-1, 21, -1]
+]
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
